11a09124149ebc21aaacde8dad978468f00ce211
Ahmed Shifa <ahmedshifa298@gmail.com>
2023-03-23 22:30:33 -0400

    Add files via upload

---
+++ App.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+
+        Ascii.printNumbers();
+        Ascii.printLowerCase();
+        Ascii.printUpperCase();
+
+
+Scanner input = new Scanner(System.in);
+System.out.print("Enter your name ");
+String name = input.next();
+System.out.println("Hi " + name);
+
+System.out.println(name + " would you like to continue? y/n");
+String contin = input.next();
+
+if (contin.equals(anObject:"y")) { contin = "y"}
+String loopAgain = "";
+while (loopAgain == "y") {
+
+System.out.println("what is the name of your pet?");
+String petName = input.next();
+
+}
+
+
+// Ask the user for the name of their favorite pet.  
+System.out.println("What is the age of your favorite pet?");
+String petAge = in.nextInt();
+
+
+
+//Get the age of the user's lucky number.
+System.out.println("What is your lucky number?");
+luckyNum = in.nextInt();
+
+//Get the favorite quarterback's jersey number.
+System.out.println("Do you have a favorite quarterback? (Yes, No)");
+in.nextLine();
+String answer = in.nextLine();
+if(answer.equalsIgnoreCase("Y") || answer.equalsIgnoreCase("Yes")){
+    System.out.println("What is the jersey number of your favorite quarterback?");
+    jerseyNum = in.nextInt();
+}
+
+
+    }
+}
+
