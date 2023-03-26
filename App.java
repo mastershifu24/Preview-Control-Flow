88532b88822eeb7e62c74956e7650b87d98fbba7
Ahmed Shifa <ahmedshifa298@gmail.com>
2023-03-26 17:46:13 -0400

    New changes

--- App.java
+++ App.java
@@ -1,52 +1,144 @@
 import java.util.Scanner;
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        
+import java.util.ArrayList;
+import java.util.Random;
 
+public class App {
+
+    static String petName;
+    static String mySurvey;
+    static int petAge;
+    static int LuckyNum;
+    static int jerseyNum;
+    static int modelYear;
+    static String actorName;
+    static int randomNumber;
+    // String name performSurvey;
+
+    
+
+    static int randNum1;
+    static int randNum2;
+    static int randNum3;
+
+    static int MAX_BALL_NUM;
+    static int MAX_LOTTERY_NUM;
+
+
+    public static <Starting> void main(String[] args) throws Exception {
         Ascii.printNumbers();
         Ascii.printLowerCase();
         Ascii.printUpperCase();
+    
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name ");
+        String name = input.next();
+        System.out.println("Hi " + name);
+
+        System.out.println("Would you like to participate in a brief survey? (Enter y or yes to continue)");
+       //String theanswer = input.nextLine();
+       //if(theanswer.equalsIgnoreCase("y") || theanswer.equalsIgnoreCase("Yes")){
+
+       // }
+
+       // public static void performSurvey(Scanner in){
+
+       // }
+        
+       String mySurvey = input.next();
+
+       System.out.println("\nWould you like to answer questions to generate another set of numbers? {Enter y or yes to continue}");
+       String response = input.next();
+
+       
+        // else{
+        //     System.out.println("Thank you for participating in the survey.\n");
+        //     quit = true;
+        // }
+    //}
 
 
-Scanner input = new Scanner(System.in);
-System.out.print("Enter your name ");
-String name = input.next();
-System.out.println("Hi " + name);
+        System.out.println(name + " would you like to continue? y/n");
+        String contin = input.next();
 
-System.out.println(name + " would you like to continue? y/n");
-String contin = input.next();
+        System.out.println("What is the name of your pet?");
+        String petName = input.next();
 
-if (contin.equals(anObject:"y")) { contin = "y"}
-String loopAgain = "";
-while (loopAgain == "y") {
+        System.out.println("how old is your pet?");
+        int petAge = input.nextInt();
 
-System.out.println("what is the name of your pet?");
-String petName = input.next();
+        System.out.println("Do you have a red car?");
+        String redCar = input.next();
 
-}
+        System.out.println("What is your lucky number?");
+        String luckyNum = input.next();
+
+        //Get your favorite quarterback's jersey number
+       System.out.println("Do you have a favorite quarterback? (yes/no))");
+        input.nextLine();
+       String answer = input.nextLine();
+        if(answer.equalsIgnoreCase("Y") || answer.equalsIgnoreCase("Yes")){
+        System.out.println("What is the jersey number?");
+        jerseyNum = input.nextInt();
+        }
+        
+
+        System.out.println("What is two-digit model year of their car?");
+        int modelYear = input.nextInt();
+
+        
+        System.out.println("Where are you from?");
+        String origin = input.next();
+
+        System.out.println("What is the first name of your favorite actor or actress?");
+        actorName = input.next();
+
+        System.out.println("Enter a random number between 1 and 50.");
+        int randomNumber = input.nextInt();
+
+        //This function will return a random number between 0 and the number specified
+        // by the input parameter range
+
+        // Starting the survey
+        boolean quit = false;
+        while(quit == false){
+
+            Random random = new Random(); 
+            int number1 = 1 + (random.nextInt(65));
+            System.out.println(number1);
+            int number2 = 1 + (random.nextInt(65));
+            int number3 = 1 + (random.nextInt(65));
+            int number4 = 1 + (random.nextInt(65));
+            int number5 = 1 + (random.nextInt(65));
+            System.out.println("Your powerball numbers are " + number1 + " " + number2 + " " + number3 + " "
+            + number4 + " " + number5);
+            char charAt =  petName.charAt(2) ;
+            int a = (charAt * number2)%75;
+            System.out.println(a);
 
 
-// Ask the user for the name of their favorite pet.  
-System.out.println("What is the age of your favorite pet?");
-String petAge = in.nextInt();
+            System.out.println("Would you like to generate more numbers? True/False");
+            quit = !input.nextBoolean();
+        }
 
-
-
-//Get the age of the user's lucky number.
-System.out.println("What is your lucky number?");
-luckyNum = in.nextInt();
-
-//Get the favorite quarterback's jersey number.
-System.out.println("Do you have a favorite quarterback? (Yes, No)");
-in.nextLine();
-String answer = in.nextLine();
-if(answer.equalsIgnoreCase("Y") || answer.equalsIgnoreCase("Yes")){
-    System.out.println("What is the jersey number of your favorite quarterback?");
-    jerseyNum = in.nextInt();
-}
+        // }
+        // } else {
+        //     System.out.println("please return later to complete the survey.");
+        // }
 
 
     }
-}
 
+
+    public static int getRandomNum(int range){
+        //create instance of Random class
+        Random rand = new Random();
+        int randNum = rand.nextInt(range);
+        return randNum;
+    }
+
+
+    private static void generateTheMagicNumbers() {
+    }
+}
--- Ascii.java
+++ Ascii.java
@@ -1,25 +1,38 @@
+import java.util.Scanner;
+
 public class Ascii {
+
+    Scanner input = new Scanner(System.in);
+
+    int userInput = input.nextInt();
+
     public static void printNumbers(){
 
-        for (int i = 48; i <= 57; i++){  
-        System.out.println(" The ASCII value for" + (char)i + "  =  " + i);  
+        for(int i = 48; i <= 57; i++){
+            char c = (char) i;
+            System.out.println(c);
         }
-        
-        }
-        
-public static void printLowerCase(){
-
-for(int i = 97; i <= 122; i++){  
-System.out.println(" The ASCII value of " + (char)i + "  =  " + i);  
-}
-
-}
-
-public static void printUpperCase(){
-
-for(int i = 65; i <= 90; i++){  
-    System.out.println(" ASCII value  " + (char)i + "  =  " + i);     
     }
 
-} 
-}
+    public static void printLowerCase(){
+
+        for(int i = 97; i <= 122; i++){
+            char d = (char) i;
+            System.out.println(d);
+        }
+
+    }
+
+    public static void printUpperCase(){
+
+        for(int i = 65; i <= 90; i++){
+            char e = (char) i;
+            System.out.println(e);
+        }
+    }
+
+    public static void petName(){
+        System.out.println("Name of your pet?");
+    }
+
+}  
